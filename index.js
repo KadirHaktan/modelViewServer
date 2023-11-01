ba1c26b0fc4b0151f4122244716275cdffc4ffd3
KadirHaktan <kadirhaktan.yucesoy@gmail.com>
2023-11-01 15:31:08 +0300

    ready to push

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const app = express();
+const port = 3010;
+
+
+const fs=require('fs')
+
+// const viewPath="C:/Users/HaktanDevice/OneDrive - HRSP/Desktop/3D Catalog Web Application/P23001/server/Presentation/wwwroot"
+
+
+const viewPath="C:/home/site/wwwroot/wwwroot"
+
+app.use(express.static(`${viewPath}`))
+
+app.get("/",(req,res,next)=>{
+    const directory=`${viewPath}/index.html`
+    fs.createReadStream(directory).pipe(res);
+})
+
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda çalışıyor.`);
+});
