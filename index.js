9f57ef2435634e3d7114c70224cd4aeb0c3eb164
KadirHaktan <kadirhaktan.yucesoy@gmail.com>
2023-11-02 12:44:48 +0300

    cors changing

--- index.js
+++ index.js
@@ -2,10 +2,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+const cors = require('cors');
 
 // Body-parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+
+app.use(cors());
+
+// veya sadece belirli bir kaynaktan gelen isteklere izin vermek için:
+app.use(cors({
+  origin: 'https://sync3dcatalog.netlify.app'
+}));
 
 // POST route
 app.post('/show-html', (req, res) => {
