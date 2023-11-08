f5cfeeb76eb283a78d60ae6a08baf347b75e7ff6
KadirHaktan <kadirhaktan.yucesoy@gmail.com>
2023-11-08 09:31:51 +0300

    changing and queue

--- index.js
+++ index.js
@@ -4,20 +4,51 @@ const app = express();
 const port = 3010;
 
 
-const fs=require('fs')
 
-let viewPath=""
-
+const amqp=require('amqplib')
 
 
-app.get("/:filePath",(req,res,next)=>{
-    viewPath+=req.params.filePath
-    const directory=`${viewPath}`
-    app.use(express.static(directory))
-    console.log(directory)
+const directoryName=__dirname
+console.log(directoryName)
+
+
+app.get('/',async(req,res,next)=>{
+    await Send({
+        directoryName
+    },"downloadPathInfo")
+
+    res.send(200)
+})
+
+
+app.get('/show',async(req,res,next)=>{
+    const directory=`${directoryName}/index.html`
     fs.createReadStream(directory).pipe(res);
 })
 
 app.listen(port, () => {
   console.log(`Sunucu ${port} portunda çalışıyor.`);
 });
+
+
+
+async function Send(data, channelName) {
+    const connection = await amqp.connect(
+        'amqps://asylnloi:X0SDax_OxfphJtZlP4WEMkSlKvC6ShWr@sparrow.rmq.cloudamqp.com/asylnloi'
+    );
+  
+    const channel = await connection.createChannel();
+    await channel.assertQueue(channelName, { durable: true });
+  
+    const message = JSON.stringify(data);
+    const messageBody = Buffer.from(message, 'utf-8');
+  
+    channel.sendToQueue(channelName, messageBody);
+  
+    console.log(`Mesaj gönderildi: ${message}`);
+  
+    await channel.close();
+    await connection.close();
+  }
+  
+  
